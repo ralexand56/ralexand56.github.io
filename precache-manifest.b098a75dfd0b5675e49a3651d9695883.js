ffdeb6d33333d8a8f624b2bfacb2bb9489d555e7
Rico Alexander <ralexand56@live.com>
2018-10-05 09:37:51 +0300

    reworked home page

---
+++ precache-manifest.b098a75dfd0b5675e49a3651d9695883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b94a00f11d89f53f0479",
+    "url": "/static/js/main.b94a00f1.chunk.js"
+  },
+  {
+    "revision": "41e70e0bbea67455f7ba",
+    "url": "/static/js/1.41e70e0b.chunk.js"
+  },
+  {
+    "revision": "b94a00f11d89f53f0479",
+    "url": "/static/css/main.cacf89cd.chunk.css"
+  },
+  {
+    "revision": "08301aa9401db25f3b135c3f193ee56a",
+    "url": "/index.html"
+  }
+];
