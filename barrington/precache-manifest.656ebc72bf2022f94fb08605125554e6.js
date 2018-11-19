953263586bd15326e40899ebd0de34bb16e64416
Rico Alexander <ralexand56@live.com>
2018-11-19 11:00:46 -0800

    monday upload removing old videos

--- barrington/precache-manifest.656ebc72bf2022f94fb08605125554e6.js
+++ barrington/precache-manifest.656ebc72bf2022f94fb08605125554e6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/barrington/static/js/runtime~main.ef00f098.js"
   },
   {
-    "revision": "b72a249521e3138b3c5e",
-    "url": "/barrington/static/js/main.b72a2495.chunk.js"
+    "revision": "8bbefd5b407edc1fb26f",
+    "url": "/barrington/static/js/main.8bbefd5b.chunk.js"
   },
   {
     "revision": "b73496d05f3b01f42e7d",
     "url": "/barrington/static/js/1.b73496d0.chunk.js"
   },
   {
-    "revision": "b72a249521e3138b3c5e",
+    "revision": "8bbefd5b407edc1fb26f",
     "url": "/barrington/static/css/main.7b1d3aff.chunk.css"
   },
   {
-    "revision": "5d324af99483539c4c6eb254b1b238cf",
+    "revision": "f32f20c8199a040dd3628cedd452a507",
     "url": "/barrington/index.html"
   }
 ];
--- barrington/service-worker.js
+++ barrington/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/barrington/precache-manifest.d6b9443417d468e416887f3de30ac80c.js"
+  "/barrington/precache-manifest.656ebc72bf2022f94fb08605125554e6.js"
 );
 
 workbox.clientsClaim();
